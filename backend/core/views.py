fb83772c5b6a1db67332656b548d29d93b0150ea
xyferr <rohit22153105@akgec.ac.in>
2025-07-12 12:31:07 +0530

    initiated the project structure

---
+++ backend/core/views.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class HelloView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, ReWear!"})
